417c610f2fd43f200ff9099679e78f74fe6c32ea
TarikFERTAHI <fertahitarik@gmail.com>
2022-11-18 14:03:52 -0800

    add order components

---
+++ frontend/src/app/orders/orders.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+  orders: any;
+  customerID!:number;
+  constructor(private http:HttpClient, private router:Router, private route:ActivatedRoute) {
+    this.customerID = route.snapshot.params['customerID'];
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:9999/order-service/orders/search/byCustomerID?projection=fullOrder&customerID="+this.customerID)
+      .subscribe({
+      next : (data)=>{
+        this.orders = data;
+      },
+      error:(err)=>{}
+    });
+  }
+
+  getOrdersDetails(o: any) {
+    this.router.navigateByUrl("/order-details/"+o.id);
+  }
+}
