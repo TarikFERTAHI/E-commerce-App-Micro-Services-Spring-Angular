fe91420af21122d566f584a1f57d5c85be633d3b
TarikFERTAHI <fertahitarik@gmail.com>
2022-11-18 15:34:38 -0800

    add Total Amount

--- order-service/src/main/java/ma/tarik/orderservice/entities/Order.java
+++ order-service/src/main/java/ma/tarik/orderservice/entities/Order.java
@@ -28,5 +28,14 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<ProductItem> productItems;
 
+    public double getTotal() {
+        double somme = 0;
+        for (ProductItem pi : productItems) {
+            somme += pi.getAmount();
+
+        }
+        return somme;
+    }
+
 
 }
