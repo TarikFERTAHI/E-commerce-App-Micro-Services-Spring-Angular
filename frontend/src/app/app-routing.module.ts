7cd9b86c62ab06eebea823037320bc69380b552f
TarikFERTAHI <fertahitarik@gmail.com>
2022-11-18 14:04:24 -0800

    edit app components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./products/products.component";
+import {CustomersComponent} from "./customers/customers.component";
+import {OrdersComponent} from "./orders/orders.component";
 
 const routes: Routes = [
   {
     path : "products", component: ProductsComponent
-  }
+  },
+  {
+    path : "customers", component: CustomersComponent
+  },
+  {
+    path : "orders/:customerID", component: OrdersComponent
+  },
+  {
+    path : "order-details/:orderId", component : OrderDetailsComponent
+  },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import {HttpClientModule} from "@angular/common/http";
 import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    CustomersComponent
+    CustomersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
