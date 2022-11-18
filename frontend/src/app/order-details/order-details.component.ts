56913887c931956bf9c0f18ed2513c06ff7005a3
TarikFERTAHI <fertahitarik@gmail.com>
2022-11-18 15:36:38 -0800

    edit order-details

--- frontend/src/app/order-details/order-details.component.ts
+++ frontend/src/app/order-details/order-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-order-details',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.css']
 })
 export class OrderDetailsComponent implements OnInit {
-
-  constructor() { }
+  orderDetails: any;
+  orderId!: number;
+  constructor(private http:HttpClient, private router:Router, private route:ActivatedRoute) {
+    this.orderId = route.snapshot.params['orderId'];
+  }
 
   ngOnInit(): void {
+  this.http.get("http://localhost:9999/order-service/fullOrder/"+this.orderId).subscribe({
+                                                                                                        next : (data)=>{
+  this.orderDetails = data;
+},
+error:(err)=>{}
+});
   }
 
 }
